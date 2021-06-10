1779446b18c98336b2f0c9af2dffc1eef4cc9637
Mas Fajar <67534306+fajarmhr@users.noreply.github.com>
2021-06-10 11:55:12 +0700

    update script

--- shutdown_server.sh
+++ shutdown_server.sh
@@ -2,19 +2,24 @@
 source /home/c0121255/fastAPI/env/bin/activate
 cd /home/c0121255/fastAPI
 
-PID=`ps -eaf | grep 'uvicorn main:app' | grep -v grep | awk '{print $2, $8}'`
+PID=`ps -eaf | grep 'uvicorn main:app' | grep -v grep | awk '{print $2}'`
+CMD=`ps -eaf | grep 'uvicorn main:app' | grep -v grep | awk '{print $8}'`
 ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
 if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID $ADD"
+  echo "killing $PID $CMD $ADD"
   kill -9 $PID
 else
 echo "FastAPI is not running"
 fi
 
-PID=`ps -eaf | grep '/home/c0121255/fastAPI/env/bin/python -c from multiprocessing' | grep -v grep | awk '{print $2, $8}'`
-ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
+PID=`ps -eaf | grep '/home/c0121255/fastAPI/env/bin/python -c from multiprocessing' | grep -v grep | awk '{print $2}'`
+CMD=`ps -eaf | grep '/home/c0121255/fastAPI/env/bin/python -c from multiprocessing' | grep -v grep | awk '{print $8}'`
+while [ "" !=  "$PID" ]; do
+  ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
+done
+
 if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID $ADD"
+  echo "killing $PID $CMD $ADD"
   kill -9 $PID
 else
 echo "FastAPI is not running"
