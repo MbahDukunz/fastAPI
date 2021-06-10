e985172e6faac525b4e278d61644754cab54a8ec
Mas Fajar <67534306+fajarmhr@users.noreply.github.com>
2021-06-10 11:59:50 +0700

    update script

--- shutdown_server.sh
+++ shutdown_server.sh
@@ -4,25 +4,22 @@ cd /home/c0121255/fastAPI
 
 PID=`ps -eaf | grep 'uvicorn main:app' | grep -v grep | awk '{print $2}'`
 CMD=`ps -eaf | grep 'uvicorn main:app' | grep -v grep | awk '{print $8}'`
-ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
 if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID $CMD $ADD"
-  kill -9 $PID
+	ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
+	echo "killing $PID $CMD $ADD"
+	kill -9 $PID
 else
-echo "FastAPI is not running"
+	echo "FastAPI is not running"
 fi
 
 PID=`ps -eaf | grep '/home/c0121255/fastAPI/env/bin/python -c from multiprocessing' | grep -v grep | awk '{print $2}'`
 CMD=`ps -eaf | grep '/home/c0121255/fastAPI/env/bin/python -c from multiprocessing' | grep -v grep | awk '{print $8}'`
-while [ "" !=  "$PID" ]; do
-  ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
-done
-
 if [[ "" !=  "$PID" ]]; then
-  echo "killing $PID $CMD $ADD"
-  kill -9 $PID
+	ADD=`netstat -tulpn | grep  $PID | grep -v grep | awk '{print $4}'`
+	echo "killing $PID $CMD $ADD"
+	kill -9 $PID
 else
-echo "FastAPI is not running"
+	echo "FastAPI is not running"
 fi
 
 sleep 2
